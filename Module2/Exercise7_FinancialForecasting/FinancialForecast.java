75cb1f6eb59797d4bf047008217cbbdca17e79c8
Mrudula Sonawane <sonawanemrudula5@gmail.com>
2026-06-22 16:30:56 +0530

    Add files via upload

---
+++ Module2/Exercise7_FinancialForecasting/FinancialForecast.java
@@ -0,0 +1,16 @@
+package Exercise7_FinancialForecasting;
+
+public class FinancialForecast {
+
+    // Recursive method
+    public static double futureValue(double currentValue, double growthRate, int years) {
+
+        // Base Case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive Call
+        return futureValue(currentValue, growthRate, years - 1) * (1 + growthRate);
+    }
+}
---
+++ Module2/Exercise7_FinancialForecasting/TestForecast.java
@@ -0,0 +1,19 @@
+package Exercise7_FinancialForecasting;
+
+public class TestForecast {
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10;   //10%
+        int years = 5;
+
+        double future =
+                FinancialForecast.futureValue(currentValue, growthRate, years);
+
+        System.out.println("Current Value : " + currentValue);
+        System.out.println("Growth Rate   : " + (growthRate * 100) + "%");
+        System.out.println("Years         : " + years);
+        System.out.println("Future Value  : " + future);
+    }
+}
